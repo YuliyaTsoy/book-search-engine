88894388568d96b187c54ee2dbfe4c428037f913
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-07 20:36:58 -0500

    modified searchBooks.js

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,11 +9,12 @@ import {
 } from "react-bootstrap";
 
 import Auth from "../utils/auth";
+import { searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
 // import Apollo hook and mutation
 import { SAVE_BOOK } from "../utils/mutations";
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation } from "@apollo/client";;
 
 const SearchBooks = () => {
   // create state for holding returned google api data
