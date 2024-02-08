0c8851facb2e4244a4ca4283a1ed41d01e3849e0
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-07 19:57:33 -0500

    refactored loginForm

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,19 +1,24 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+// refactored to use graphql API instead of RESTful API
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
+   // setting loginUser stateful variable with useMutation
+   const [loginUser, { error }] = useMutation(LOGIN_USER);
+
+   useEffect(() => {
+     if (error) setShowAlert(true);
+     else setShowAlert(false);
+   }, [error])
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -25,19 +30,15 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
+    // use loginUser function
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
     }
 
     setUserFormData({
