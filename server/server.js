0c574685ed30d7bb8215fecd6a0722d240a85bd9
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-07 22:19:56 -0500

    refactored vite config

--- server/server.js
+++ server/server.js
@@ -7,32 +7,30 @@ const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-
 const app = express();
-const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-
-// starts Apollo server:
-const startApolloServer = async () => {
-  await server.start();
-
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  // use middleware to connect to graphql end point
-  app.use('/graphql', expressMiddleware(server));
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client'));
     });
   }
+
+  const startApolloServer = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+
   db.once('open', () => {
     app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
