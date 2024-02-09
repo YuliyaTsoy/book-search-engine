9eff7999fe3cb3c603a56e5e5fea8f64a75d443c
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-09 00:18:56 -0500

    fixed impossible bug in auth.js on server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -11,7 +11,7 @@ module.exports = {
     },
   }),
   // function for our authenticated routes
-  authMiddleware: function (req) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
