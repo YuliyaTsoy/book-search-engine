9a08e7a82df666466f977a86ebd1380f5c7cfafb
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-08 19:46:01 -0500

    fixed bugs in mutations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,6 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 // Construct main graphql endpoint
