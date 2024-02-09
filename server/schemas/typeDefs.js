dfb19715e9177a12636a0183f63835de4539ef78
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-09 09:57:40 -0500

    removed exclamation mark in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,36 +1,33 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type User {
-  _id: ID!
-  username: String!
-  email: String
-  bookCount: Int
-  savedBooks: [Book]
-}
-type Book {
-  bookId: ID!
-  authors: [String]
-  description: String
-  title: String!
-  image: String
-  link: String
-}
-type Auth {
-  token: ID!
-  user: User
-}
-input BookInput {
-authors: [String]
-description: String!
-title: String!
-bookId: String!
-image: String
-link: String
-}
-
-
-
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type BookInput {
+    authors: [String]
+    description: String
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
 
   type Query {
     me: User
